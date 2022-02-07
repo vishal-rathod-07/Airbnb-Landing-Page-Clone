ceb6e7b99ac34fc1a7d8792f10457ca64df0c0c3
Vishal Rathod <rvd31305@gmail.com>
2022-02-07 14:27:21 +0530

    Added js for navigation button, scroll cards and bottom navigation added
    - Navigation buttons on click effect
    - on Scroll card navigation buttons status 
    - on scroll bottom navigation animations

---
+++ main.js
@@ -0,0 +1,40 @@
+// city card scroll buttons
+const cardScrollView = document.getElementById("city-scroll-cards");
+const btnPrevious = document.getElementById("btn-previous");
+const btnNext = document.getElementById("btn-next");
+
+btnNext.onclick = function () {
+  cardScrollView.scrollLeft += 150;
+};
+
+btnPrevious.onclick = function () {
+  cardScrollView.scrollLeft -= 150;
+};
+
+cardScrollView.addEventListener("scroll", function () {
+  if (cardScrollView.scrollLeft === 0) {
+    btnPrevious.disabled = true;
+  } else {
+    btnPrevious.disabled = false;
+  }
+  if (
+    cardScrollView.scrollLeft ===
+    cardScrollView.scrollWidth - cardScrollView.offsetWidth
+  ) {
+    btnNext.disabled = true;
+  } else {
+    btnNext.disabled = false;
+  }
+});
+
+// bottom navigation
+var prevScrollpos = window.pageYOffset;
+window.onscroll = function () {
+  var currentScrollPos = window.pageYOffset;
+  if (prevScrollpos > currentScrollPos) {
+    document.getElementById("bottom-nav").style.bottom = "0";
+  } else {
+    document.getElementById("bottom-nav").style.bottom = "-65px";
+  }
+  prevScrollpos = currentScrollPos;
+};
